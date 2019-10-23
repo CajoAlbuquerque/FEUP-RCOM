6124d1cbfcb67fedede76cd6221ea1f252b7a1a2
root <ferreiraleninha52@gmail.com>
2019-10-23 16:59:06 +0100

    continue receiver in application layer

--- Projeto1/application/application.c
+++ Projeto1/application/application.c
@@ -2,14 +2,16 @@
 #include "../protocol/protocol.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
 unsigned char* getCharBuffer(unsigned char *filename, int *fileSize){
     FILE *f;
     unsigned char *fileData;
     struct stat fData;
-    if((f = open((char *) filename, "rb")) == NULL)
+    if((f = fopen((char *) filename, "rb")) == NULL)
     {
         perror("Error opening the file :");
         exit(-1);
@@ -39,17 +41,44 @@ unsigned char* dataPacket(int sendSize, int sequenceNumber, unsigned char* filen
     fileData[3] = L1;
     while(count <= sendSize){
         fileData[4+count] = filename + count;
+        count++;
     }
 
     return fileData;
 }
 
+int receiveFile(){
+    int L1, L2, counter;
+    int fileSize, receiveSize = 0, sequenceNumber = 0, readSize;
+    unsigned char *fileData, *dataReceive;
+
+    readSize = llread(application.fileDescriptor, fileData);
+    receiveSize = fileData[0];
+    sequenceNumber = fileData[1];
+    L2 = fileData[2];
+    L1 = fileData[3];
+    if(readSize != 256 *L2 + L1 ){
+        printf("Wrong Reception\n");
+        exit(-1);
+    }
+    while(readSize >= counter){
+        dataReceive[counter] = fileData[4 + counter];
+        counter++;
+    }
+    
+}
+
 int controlPacket(unsigned int control, int fileSize, unsigned char filename){
+    if(control != 2 && control != 3){
+        printf("control can't be different than 2 and 3");
+        exit(-1);
+    }
     int count = 0;
     unsigned char set[7];
 
     set[0] = control;
     set[1] = fileSize;
+    set[2] = filename;
 
  //TODO
 
@@ -61,12 +90,12 @@ int sendFile(unsigned char *filename){
     unsigned char *fileData, *dataSend;
     fileData = getCharBuffer((unsigned char*) filename, &fileSize);
     controlPacket(0x02, fileSize, filename);
-    while((fileSize - sendSize) > 200){ //max is 256
-        *dataSend = dataPacket(200, sequenceNumber, filename);
+    while((fileSize - sendSize) > 150){ //if possible sends 150 bits of data
+        *dataSend = dataPacket(150, sequenceNumber, filename);
         llwrite(application.fileDescriptor, dataSend, &fileSize);
         free(dataSend);
     } 
-    if((fileSize - sendSize) > 0){ //max is 256
+    if((fileSize - sendSize) > 0){ 
         *dataSend = dataPacket((fileSize - sendSize), sequenceNumber, filename);
         llwrite(application.fileDescriptor, dataSend, &fileSize);
         free(dataSend);
@@ -75,8 +104,9 @@ int sendFile(unsigned char *filename){
     controlPacket(0x03, fileSize, filename);
 }
 
+
 int main(int argc, char **argv){
-    unsigned char *fileData;
+    
     application.status = argv[2];
 
     if ((argc < 2) || ((strcmp("/dev/ttyS0", argv[1]) != 0) &&
@@ -95,13 +125,13 @@ int main(int argc, char **argv){
         exit(1);
     }
 
-    application.fileDescriptor = llopen(argv[0], argv[1]);
+    application.fileDescriptor = llopen(argv[1], application.status);
 
-    if(strcmp("TRANSMITTER",argv[2]) == 0){
+    if(strcmp("TRANSMITTER", application.status) == 0){
         sendFile(argv[3]);
         llclose(application.fileDescriptor);
     } else {
-        llread(application.fileDescriptor, fileData);
+        receiveFile();
     }
     
     printf("Sucessfull transmition\n");
