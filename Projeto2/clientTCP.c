35419d26e7d0c0cc13bae11eb2a68b8424c22b02
MHelena45 <ferreiraleninha52@gmail.com>
2019-11-28 12:59:45 +0000

    start download

---
+++ Projeto2/clientTCP.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <netdb.h>
+#include <string.h>
+
+#define MAX_SIZE 256
+
+int open_socket(int port, char *address)
+{
+	int sockfd;
+	struct sockaddr_in server_addr;
+
+	/*server address handling*/
+	bzero((char *)&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(address); /*32 bit Internet address network byte ordered*/
+	server_addr.sin_port = htons(port);			  /*server TCP port must be network byte ordered */
+
+	/*open an TCP socket*/
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("socket()");
+		return -1;
+	}
+	/*connect to the server*/
+	if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	{
+		perror("connect()");
+		return -1;
+	}
+
+	return sockfd;
+}
+
+int configure_server(int socket, const char *user,const char *pass){
+	char cmd[MAX_SIZE];
+	int bytes, cat_result;
+
+	cat_result = sprintf(cmd, "user %s", user);
+	bytes = write(socket, cmd, cat_result + 1);
+	if(bytes < 0){
+		perror("writing user");
+		return -1;
+	}
+
+	bytes = read(socket, cmd, MAX_SIZE);
+	if(bytes < 0){
+		perror("reading user");
+		return -1;
+	}
+
+	printf("Response : %s\n",cmd);
+
+	cat_result = sprintf(cmd, "pass %s", pass);
+	bytes = write(socket, cmd, cat_result + 1);
+	if(bytes < 0){
+		perror("writing password");
+		return -1;
+	}
+
+	bytes = read(socket, cmd, MAX_SIZE);
+	if(bytes < 0){
+		perror("reading pass");
+		return -1;
+	}
+
+	printf("Response : %s\n",cmd);
+
+	// cmd = "pasv";
+	// bytes = write(socket, cmd, sizeof(cmd));
+	// if(bytes < 0){
+	// 	perror("writing pasv");
+	// 	return -1;
+	// }
+
+	return 0;
+}
+
+int close_socket(int socket) {
+	close(socket);
+	return 0;
+}
---
+++ Projeto2/download.c
@@ -0,0 +1,35 @@
+#include "getip.c"
+#include "clientTCP.c"
+
+int main(int argc, char *argv[]) {
+    int socket;
+    char *ip_address;
+    char *user = "anonymous";
+    char *pass = "ga_";
+
+    if(argc != 2){
+        printf("Usage: %s ftp://[<user>:<password>@]<host>/<url-path>", argv[0]);
+        return -1;
+    }
+    printf("Nice arguments bro!\n");
+    // Coneverting host name to ip address
+    ip_address = getip(argv[1]); //TODO: parse argv[1]
+    printf("IP = %s\n", ip_address);
+
+    // Opening socket and connection to server
+    socket = open_socket(21, ip_address);
+    printf("WOW that socket mate! It is %d\n", socket);
+    if(socket < 0) {
+        return -1;
+    }
+
+    if(configure_server(socket, user, pass) < 0) {
+        return -1;
+    }
+    
+    printf("It configured CRL!\n");
+
+    close_socket(socket);
+
+    return 0;
+}
