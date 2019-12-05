1f1ea6c11b1bfc7c636f7028ec9fb50adb4c78a7
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-12-05 15:06:16 +0000

    Code formating

--- Projeto2/clientTCP.c
+++ Projeto2/clientTCP.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define MAX_SIZE 256
 
@@ -21,7 +21,7 @@ int open_socket(int port, char *address)
 	bzero((char *)&server_addr, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = inet_addr(address); /*32 bit Internet address network byte ordered*/
-	server_addr.sin_port = htons(port);			  /*server TCP port must be network byte ordered */
+	server_addr.sin_port = htons(port);				  /*server TCP port must be network byte ordered */
 
 	/*open an TCP socket*/
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -39,24 +39,27 @@ int open_socket(int port, char *address)
 	return sockfd;
 }
 
-int parse_ip_port(const char * line, char * ip) {
-	char * raw = strchr(line, '(');
+int parse_ip_port(const char *line, char *ip)
+{
+	char *raw = strchr(line, '(');
 	int length = strlen(raw);
 	char values[length];
 	char nums[6][4];
 	char *token;
-	
+
 	strncpy(values, raw + 1, length - 5);
-   
+
 	token = strtok(values, ",");
 	strcpy(nums[0], token);
 
-  	for(int i = 1; i < 6; i++) {
+	for (int i = 1; i < 6; i++)
+	{
 		token = strtok(NULL, ",");
 		strcpy(nums[i], token);
-   	}
-	
-	if(sprintf(ip, "%s.%s.%s.%s", nums[0], nums[1], nums[2], nums[3]) < -1){
+	}
+
+	if (sprintf(ip, "%s.%s.%s.%s", nums[0], nums[1], nums[2], nums[3]) < -1)
+	{
 		perror("Constructing response IP");
 		return -1;
 	}
@@ -64,17 +67,21 @@ int parse_ip_port(const char * line, char * ip) {
 	return atoi(nums[4]) * 256 + atoi(nums[5]);
 }
 
-int get_ip_port(int fd, char * ip) {
-	FILE* socket = fdopen(fd, "r");
+int get_ip_port(int fd, char *ip)
+{
+	FILE *socket = fdopen(fd, "r");
 	char buf[MAX_SIZE], code[4]; //code is the line first 3 digits
 
-	if(socket == NULL){
+	if (socket == NULL)
+	{
 		perror("Opening file descriptor");
 		return -1;
 	}
 
-	while(1) {
-		if(fgets(buf, MAX_SIZE, socket) == NULL){
+	while (1)
+	{
+		if (fgets(buf, MAX_SIZE, socket) == NULL)
+		{
 			perror("Reading response");
 			return -1;
 		}
@@ -82,21 +89,23 @@ int get_ip_port(int fd, char * ip) {
 		strncpy(code, buf, 3);
 		code[3] = '\0';
 
-		if(strcmp(code, "227") == 0)
+		if (strcmp(code, "227") == 0)
 			break;
 	}
 
 	return parse_ip_port(buf, ip);
 }
 
-int configure_server(int socket, const char *user, const char *pass, char * ip){
+int configure_server(int socket, const char *user, const char *pass, char *ip)
+{
 	char cmd[MAX_SIZE];
 	int bytes, cat_result;
 
 	// Send user comand
 	cat_result = sprintf(cmd, "user %s\n", user);
 	bytes = write(socket, cmd, cat_result);
-	if(bytes < 0){
+	if (bytes < 0)
+	{
 		perror("Writing user");
 		return -1;
 	}
@@ -104,7 +113,8 @@ int configure_server(int socket, const char *user, const char *pass, char * ip){
 	// Send pass comand
 	cat_result = sprintf(cmd, "pass %s\n", pass);
 	bytes = write(socket, cmd, cat_result);
-	if(bytes < 0){
+	if (bytes < 0)
+	{
 		perror("Writing password");
 		return -1;
 	}
@@ -112,7 +122,8 @@ int configure_server(int socket, const char *user, const char *pass, char * ip){
 	// Send pasv comand
 	cat_result = sprintf(cmd, "pasv\n");
 	bytes = write(socket, cmd, cat_result);
-	if(bytes < 0){
+	if (bytes < 0)
+	{
 		perror("Writing pasv");
 		return -1;
 	}
@@ -120,7 +131,8 @@ int configure_server(int socket, const char *user, const char *pass, char * ip){
 	return get_ip_port(socket, ip);
 }
 
-int close_socket(int socket) {
+int close_socket(int socket)
+{
 	close(socket);
 	return 0;
 }
--- Projeto2/download.c
+++ Projeto2/download.c
@@ -1,17 +1,19 @@
-#include "getip.c"
 #include "clientTCP.c"
+#include "getip.c"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int socket, port;
     char *ip_address, pasv_ip[MAX_SIZE];
     char *user = "anonymous";
     char *pass = "ga_";
 
-    if(argc != 2){
+    if (argc != 2)
+    {
         printf("Usage: %s ftp://[<user>:<password>@]<host>/<url-path>\n", argv[0]);
         return -1;
     }
-    
+
     // Coneverting host name to ip address
     ip_address = getip(argv[1]); //TODO: parse argv[1]
     printf("Server IP address = %s\n", ip_address);
@@ -19,17 +21,19 @@ int main(int argc, char *argv[]) {
     // Opening socket and connection to server
     socket = open_socket(21, ip_address);
 
-    if(socket < 0) {
+    if (socket < 0)
+    {
         return -1;
     }
 
     printf("Configuring server...\n");
     port = configure_server(socket, user, pass, pasv_ip);
-    if(port < 0) {
+    if (port < 0)
+    {
         return -1;
     }
     printf("Port is: %d\n", port);
-    
+
     printf("It configured CRL!\n");
 
     close_socket(socket);
